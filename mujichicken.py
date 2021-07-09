fd2e4bc65136ab0dc51bc3a284f57c66e9b6e12f
mujichicken <yutoyuto0925@gmail.com>
2021-07-09 12:57:44 +0900

    a

--- mujichicken.py
+++ mujichicken.py
@@ -28,14 +28,36 @@ print(tagName)
 #いいね数を設定
 likedMax = 200
 
-options = Options()
-options.binary_location = '/app/.apt/usr/bin/google-chrome'
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
+# chrome driverの配置場所を設定
+CHROME_DRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+# ダウンロードフォルダ―の場所を設定・作成
+DOWNLOAD_DIR = '/app/tmp'
+os.mkdir(DOWNLOAD_DIR)
+
+# chromeの起動オプションを設定
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('start-maximized')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+# chromeの追加設定(driverの場所やファイル等のダウンロード先)
+prefs = {'download.default_directory': DOWNLOAD_DIR,
+         'download.prompt_for_download': False}
+options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH,
+                           options=options)
+driver.command_executor._commands['send_command'] = ('POST',
+                                                      '/session/$sessionId/chromium/send_command')
+params = {'cmd': 'Page.setDownloadBehavior',
+          'params': {'behavior': 'allow',
+                     'downloadPath': DOWNLOAD_DIR}}
+driver.execute('send_command', params)
     
 #ブラウザに接続
-driver = webdriver.Chrome(chrome_options=options)
-driver.implicitly_wait(10)
 time.sleep(5)
 
 #インスタのURLにアクセス
